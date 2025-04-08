0653c2ced5e36981fbf1f653a183a6eb59aa848b
Redux Gamer <joseccrodrigues@hotmail.com>
2025-04-08 23:57:02 +0200

    Button to let the customer enter a custom tip plus view binding

--- app/src/main/java/com/example/tipsapp/MainActivity.kt
+++ app/src/main/java/com/example/tipsapp/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.tipsapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tipsapp.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+      binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnClean.setOnClickListener {
+            println("José " + binding.tieTotal.text.toString())
+            println("Brunella " + binding.tieNumPeople.text.toString())
+        }
 
     }
 }
